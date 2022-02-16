bb79d73076a9e9918a05393e627c17bf3ed4b25b
Ajinkya <ajinkya.bhosale85@gmail.com>
2022-02-15 19:12:28 -0500

    Added method to make a complete adj^Cent graph.

---
+++ Graphs.py
@@ -0,0 +1,90 @@
+"""
+This file goes over some of the methods we used to work with Graphs.
+
+Copyright and Usage Information
+===============================
+This file is Copyright (c) 2021 Ajinkya Bhosale.
+"""
+
+from __future__ import annotations
+from typing import Any
+
+
+class _Vertex:
+    """A vertex in a graph.
+
+    Instance Attributes:
+       - item: The data stored in this vertex.
+       - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+      - self not in self.neighbours
+      - all(self in vertex.neighbours for vertex in self.neighbours)
+    """
+    item: Any
+    neighbours: set[_Vertex]
+
+    def __init__(self, item: Any, neighbours: set[_Vertex]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        self.item = item
+        self.neighbours = neighbours
+
+
+class Graph:
+    """A graph."""
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
+    def add_vertex(self, item: Any) -> None:
+        """Add a vertex with the given item to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+
+        Preconditions:
+            - item not in self._vertices
+        """
+        self._vertices[item] = _Vertex(item, set())
+
+    def add_edge(self, item1: Any, item2: Any) -> None:
+        """Add an edge between the two vertices with the given items in this graph.
+
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
+        """
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            v2 = self._vertices[item2]
+
+            # Add the new edge
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
+        else:
+            # We didn't find an existing vertex for both items.
+            raise ValueError
+
+
+def complete_graph(n: int) -> Graph:
+    """Return a graph of n vertices where all pairs of vertices are adjacent.
+
+    The vertex items are the numbers 0 through n - 1, inclusive.
+
+    Preconditions:
+        - n >= 0
+    """
+    graph = Graph()
+
+    for v in range(n):
+        graph.add_vertex(v)
+        for u in range(v):
+            graph.add_edge(v, u)
+
+    return graph
